0c62394c5aade58f989c7666be3a035f41a43622
Sajid <sajidahamed2001@gmail.com>
2025-06-28 01:07:37 +0530

    added editor components

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,11 +1,89 @@
+import { useState } from "react";
+// import { Link } from "react-router-dom";
+
 export default function SignupPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   return (
-    <div className="flex min-h-screen items-center justify-center bg-gray-50">
-      <div className="rounded-lg bg-white p-8 shadow-md">
-        <h1 className="mb-4 text-2xl font-bold text-gray-900">Signup Page</h1>
-        <p className="text-gray-700">
-          This is a placeholder for route testing.
-        </p>
+    <div className="py-12">
+      <div className="">
+        <img
+          alt=""
+          src=""
+          className="mx-auto h-10 w-auto"
+        />
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+          Sign up for an account
+        </h2>
+      </div>
+      <div className="flex justify-around bg-gray-50">
+        <div className="notes rounded-lg bg-white p-8 shadow-md">
+          <h1 className="mb-4 text-2xl font-bold text-gray-900">
+            Important Notes
+          </h1>
+          <p className="text-gray-700">
+            This is a placeholder for route testing.
+          </p>
+        </div>
+        <div className="form">
+          <form
+            // onSubmit={(e) => handleLoginSubmit(username, password, e)}
+            className="space-y-6"
+          >
+            <div>
+              <label
+                htmlFor="username"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                User name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  required
+                  autoComplete="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="password"
+                  className="block text-sm/6 font-medium text-gray-900"
+                >
+                  Password
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  required
+                  autoComplete="current-password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            {/* {error && <div className="text-red-500 text-sm">{error}</div>} */}
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
